1e7e4c3dfb406b0e23a2284eac8837bff3de26da
Anton Kutishevsky <akutishevsky@gmail.com>
2025-10-11 16:32:17 +0300

    feat: remove DevTools opening from main window

--- src/main.js
+++ src/main.js
@@ -32,9 +32,6 @@ const createWindow = () => {
             ),
         );
     }
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
